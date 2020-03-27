3d8774f8949f9dc3a6dcc4a8ea5cff7a6662c11a
Jim Filippou <jimfilippou8@gmail.com>
2020-03-28 01:40:30 +0200

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,10 @@
+import models.Client
+
+fun main() {
+
+    // Here we experiment :D
+
+    var c = Client()
+
+    println(c)
+}
---
+++ src/models/client.kt
@@ -0,0 +1,8 @@
+package models
+
+class Client {
+
+    val clientID: String? = null;
+    val followers: MutableList<Client> = mutableListOf<Client>();
+
+}
